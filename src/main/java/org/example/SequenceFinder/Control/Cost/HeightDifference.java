f33e3eedc508014fe779f055fa815ae02bf9a9a8
0xBigfish <46397578+0xBigfish@users.noreply.github.com>
2022-04-25 22:33:48 +0200

    add cost function: height difference

---
+++ src/main/java/org/example/SequenceFinder/Control/Cost/HeightDifference.java
@@ -0,0 +1,31 @@
+package org.example.SequenceFinder.Control.Cost;
+
+import org.example.SequenceFinder.Model.GeometricObjects.Box;
+
+public class HeightDifference<T extends Box> implements CostFunction<T> {
+
+    private final double cost;
+
+    /**
+     * Sets the cost per unit of height difference
+     *
+     * @param costPerUnitDiff the cost per unit of height difference
+     */
+    public HeightDifference(double costPerUnitDiff) {
+        this.cost = costPerUnitDiff;
+    }
+
+    /**
+     * {@inheritDoc} <br>
+     * <br>
+     * Returns cost set in the constructor times the height difference of nodeA and nodeB in the world
+     *
+     * @param nodeA a node in the graph
+     * @param nodeB a node with an incoming directed edge from nodeA
+     * @return cost set in the constructor times the height difference of nodeA and nodeB in the world
+     */
+    @Override
+    public double calcCost(T nodeA, T nodeB) {
+        return cost * Math.abs(nodeA.calcCenter().z - nodeB.calcCenter().z);
+    }
+}
