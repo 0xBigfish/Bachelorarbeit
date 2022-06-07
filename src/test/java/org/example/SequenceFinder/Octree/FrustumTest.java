5d32b8d384a0cfcabda1c940327e775283d5fb72
0xBigfish <46397578+0xBigfish@users.noreply.github.com>
2022-06-07 22:47:44 +0200

    add rest of the tests for the 90 degree frustum

--- src/test/java/org/example/SequenceFinder/Octree/FrustumTest.java
+++ src/test/java/org/example/SequenceFinder/Octree/FrustumTest.java
@@ -34,7 +34,7 @@ class FrustumTest {
 
 
     @Nested
-    @DisplayName("given a Frustum ")
+    @DisplayName("given a 90 degree Frustum")
     class GivenFrustum {
 
         @BeforeEach
@@ -46,9 +46,9 @@ class FrustumTest {
             Plane bottom = new Plane(new Vector3D(0, 0, -1), new Vector3D(0, 0, 1), TOLERANCE);
 
             // 45° angle
-            Plane left = new Plane(new Vector3D(2, 0, 0), new Vector3D(1, -1, 0), TOLERANCE);
+            Plane left = new Plane(new Vector3D(0, 2, 0), new Vector3D(1, -1, 0), TOLERANCE);
             // 45° angle
-            Plane right = new Plane(new Vector3D(-2, 0, 0), new Vector3D(1, 1, 0), TOLERANCE);
+            Plane right = new Plane(new Vector3D(0, -2, 0), new Vector3D(1, 1, 0), TOLERANCE);
 
             Plane front = new Plane(new Vector3D(10, 0, 0), new Vector3D(-1, 0, 0), TOLERANCE);
             Plane back = new Plane(new Vector3D(0, 0, 0), new Vector3D(1, 0, 0), TOLERANCE);
@@ -77,6 +77,17 @@ class FrustumTest {
         @DisplayName("when a box is outside the frustum")
         class BoxOutsideFrustum {
 
+            @BeforeEach
+            void setup() {
+                // behind every plane except the top plane
+                box = new ConcreteBox(new Point(-1, 3, -1.5), new Point(-0.5, 2.5, -1.25));
+            }
+
+            @Test
+            @DisplayName("then the box should not be visible")
+            void notVisible() {
+                assertEquals(NOT_VISIBLE, frustum.calcVisibility(box));
+            }
         }
 
 
@@ -84,13 +95,71 @@ class FrustumTest {
         @DisplayName("when a box intersects the frustum")
         class BoxIntersectsFrustum {
 
+            @BeforeEach
+            void setup() {
+                box = new ConcreteBox(new Point(-1, -0.5, 1.5), new Point(1, -4, 0));
+            }
+
+            @Test
+            @DisplayName("then the box should be partly visible")
+            void partlyVisible() {
+                // intersects only the right plane
+                assertEquals(PARTLY_VISIBLE, frustum.calcVisibility(box));
+            }
         }
 
 
         @Nested
-        @DisplayName("when a box intersects a plane, but is behind another plane")
-        class IntersectsOneBehindAnother {
+        @DisplayName("when a box's side lays in a frustum plane but intersects no other planes")
+        class BoxSideLaysInAPlane {
 
+            @BeforeEach
+            void setup() {
+                // half the box's vertices lay on the back plane but intersect no other planes
+                box = new ConcreteBox(new Point(0, 1, 1), new Point(1, -1, 0));
+            }
+
+            @Test
+            @DisplayName("then the box should be partly visible")
+            void partlyVisible() {
+                assertEquals(PARTLY_VISIBLE, frustum.calcVisibility(box));
+            }
+
+        }
+
+
+        @Nested
+        @DisplayName("when a box intersects two frustum planes")
+        class BoxIntersectsTwoPlanes {
+
+            @BeforeEach
+            void setup() {
+                // intersects the left plane and the back plane in the back-left corner
+                box = new ConcreteBox(new Point(8, 12, 0), new Point(12, 8, 0));
+            }
+
+            @Test
+            @DisplayName("then the box should be partly visible")
+            void partlyVisible() {
+                assertEquals(PARTLY_VISIBLE, frustum.calcVisibility(box));
+            }
+        }
+
+
+        @Nested
+        @DisplayName("when the frustum is completely inside the box")
+        class FrustumInBox {
+
+            @BeforeEach
+            void setup() {
+                box = new ConcreteBox(new Point(-5, 5, -2), new Point(15, -15, 3));
+            }
+
+            @Test
+            @DisplayName("then the box should be partly visible")
+            void notVisible() {
+                assertEquals(PARTLY_VISIBLE, frustum.calcVisibility(box));
+            }
         }
     }
 
